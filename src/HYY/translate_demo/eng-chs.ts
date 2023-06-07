b97e92ee2d81e51a75027fe21b5eef1fcb60c97c
hanyue <hanyueui@163.com>
2023-06-07 09:36:27 +0800

    .

---
+++ src/HYY/translate_demo/eng-chs.ts
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>Demo</name>
+    <message>
+        <location filename="Tra002.py" line="43"/>
+        <source>Start</source>
+        <translation type="unfinished">开始</translation>
+    </message>
+    <message>
+        <location filename="Tra002.py" line="44"/>
+        <source>Hello, World</source>
+        <translation type="unfinished">你好</translation>
+    </message>
+</context>
+</TS>
